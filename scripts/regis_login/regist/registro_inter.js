96ba80fab36793f1d2011753d13da8e3ae502164
EmiSanFlo <a21100316@ceti.mx>
2025-03-28 19:13:32 +0000

    .

--- scripts/regis_login/regist/registro_inter.js
+++ scripts/regis_login/regist/registro_inter.js
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log("Correo verificado correctamente");
 
             // Redirigir al siguiente paso de registro
-            window.location.href = `regist_inter.html?email=retry}`;
+            window.location.href = `registro_p2.html?email=${encodeURIComponent(email)}`;
         }, 3000); // Simula un retraso de 3 segundos para la verificación
     } else {
         alert("No se pudo verificar el correo. Intenta nuevamente.");
-        window.location.href = ""; // Si no hay correo en la URL, redirigir a la página anterior
+        window.location.href = "regist_inter.html?email=retry"; // Si no hay correo en la URL, redirigir a la página anterior
     }
 });
